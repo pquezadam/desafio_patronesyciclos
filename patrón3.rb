56c0073bffb867358fea81fc2750875e6891afcc
Pablo <pquezada2009@hotmail.es>
2019-05-25 14:20:35 -0400

    desafio_patronesyciclos

---
+++ patrón3.rb
@@ -0,0 +1,9 @@
+n = ARGV[0].to_i
+n.times do |i|
+    if i.even?
+        print '12'
+    else
+        print '12'
+    end
+end
+print "\n"
---
+++ patrón4.rb
@@ -0,0 +1,9 @@
+n = ARGV[0].to_i
+n.times do |i|
+    if i.even?
+        print '123'
+    else
+        print '123'
+    end
+end
+print "\n"
